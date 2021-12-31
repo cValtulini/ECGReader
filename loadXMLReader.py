96f5563440ad180c23e0a7405e79d0b7c0ebcafd
cValtulini <c.valtulini002@studenti.unibs.com>
2021-12-31 11:23:13 +0100

    Added docstring, changed path handling, added log messages

--- loadXMLReader.py
+++ loadXMLReader.py
@@ -3,14 +3,18 @@ Install the XMLReader in the `spxml` folder
 Adds the installation directory to the system path
 """
 import os
+
+print("Looking for dependencies:")
 os.system('apt-get install libxml2')
 os.system('apt-get install libxml2-dev')
 
 os.system('mv /ECGREader/spxml .')
 
+print("Installing spxml: ")
 os.chdir('spxml')
 os.system('python3  setup.py install --user')
 os.chdir('..')
 
+print("Adding installation folder to path: ")
 import sys
 sys.path.append(r'/root/.local/lib/python3.7/site-packages')
