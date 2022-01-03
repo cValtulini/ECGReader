202003728491624a269aa5543de35c60444a3ed5
cValtulini <c.valtulini002@studenti.unibs.com>
2022-01-03 21:01:11 +0100

    added notes for string parsing and directory reading + main code outline

---
+++ src/dataSorter.py
@@ -0,0 +1,22 @@
+from google.colab import drive
+import os
+
+drive.mount('/content/drive')
+
+# Notes for later
+# Parse strings
+#x = " "
+#x.split(sep='')
+
+# Scan files in a dircetory
+#os.scandir()
+
+# Main Code Execution
+
+# Get XML files list
+# Rename XML files
+
+# Get PDF files
+# Rename PDF files
+
+# Find association name + data
