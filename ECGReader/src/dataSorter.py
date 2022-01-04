9aef420565dc6dce6f5c21e642c6716210cdabb4
cValtulini <c.valtulini002@studenti.unibs.com>
2022-01-04 21:55:21 +0100

    test

--- ECGReader/src/dataSorter.py
+++ ECGReader/src/dataSorter.py
@@ -242,3 +242,5 @@ if __name__ == '__main__':
 
     # Find matches between xml / jpg and organize files
     # matchesFinder('content/data/jpg', 'content/data/xml')
+
+    print('Hello, Universe!')
