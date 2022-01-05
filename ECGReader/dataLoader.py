7d3f571ee79ae50601803fd414a79304dad379e7
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-05 11:36:54 +0100

    modified dataLoader.py

--- ECGReader/dataLoader.py
+++ ECGReader/dataLoader.py
@@ -1,11 +1,14 @@
 """
 Class/Utility to load data for the Net
 """
-#import numpy as np
-#import pandas as pd
+import numpy as np
+import pandas as pd
 #import SPXml
-#import tensorflow as tf
-#from tensorflow import Keras
+import tensorflow as tf
+from tensorflow import Keras
 
-
+class DataLoader(object):
+    
+    def __init__(bar):
+        self.foo = bar
 
