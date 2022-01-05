98bac67f57d89fed4abe1659ea3bb236a9901a53
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-05 13:10:16 +0100

    modification to SPxml installation (prova)

--- ECGReader/ECGLoader.py
+++ ECGReader/ECGLoader.py
@@ -1,14 +1,12 @@
 """
 Class/Utility to load data for the Net
 """
-import cv2
 import numpy as np
 import pandas as pd
-#import SPXml
 import tensorflow as tf
 from tensorflow import keras
 
-class DataLoader(object):
+class ECGLoader(object):
     
     def __init__(self, bar):
         self.foo = bar
--- ECGReader/src/loadXMLReader.py
+++ ECGReader/src/loadXMLReader.py
@@ -8,9 +8,9 @@ os.system('mv ./ECGReader/spxml .')
 
 print("Installing spxml: ")
 os.chdir('spxml')
-os.system('python3  setup.py install --user')
+os.system('python3  setup.py install')
 os.chdir('..')
 
-print("Adding installation folder to path: ")
-import sys
-sys.path.append(r'/root/.local/lib/python3.7/site-packages')
+#print("Adding installation folder to path: ")
+#import sys
+#sys.path.append(r'/root/.local/lib/python3.7/site-packages')
