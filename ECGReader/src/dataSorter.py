2af952ba629a663f91ee3d4d8d976f0703ed5c60
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-05 22:37:50 +0100

    ready for high res png

--- ECGReader/src/dataSorter.py
+++ ECGReader/src/dataSorter.py
@@ -327,7 +327,7 @@ def imagePreProcess(path):
     print('Pre-processing PNG images...')
 
     # Left/Upper Right/Lower coordinates of the cropping box
-    crop_vertices = (38, 203, 767, 560)
+    crop_vertices = (476, 2540, 9588, 6990)
 
     for file in matches_files:
         with Image.open(file.path) as img:
@@ -348,7 +348,7 @@ def imagePreProcess(path):
 
 
 if __name__ == '__main__':
-    _, data_path, dpi = argv
+    _, data_path = argv
 
     # Copy data into local content folder
     os.system(f'cp -r {data_path} /content/')
@@ -360,7 +360,7 @@ if __name__ == '__main__':
     renamePDFFiles('/content/data/pdf')
     
     # Convert PDF files to PNG
-    convertPdfToPng(f'/content/data', resolution=dpi)
+    convertPdfToPng(f'/content/data', resolution=900)
 
     # Find matches between xml / png and organize files
     matchesFinder('/content/data/png', '/content/data/xml')
