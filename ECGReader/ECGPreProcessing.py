b339f013891a79c534b5eb5056c3edc4838a636a
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-05 21:36:37 +0100

    pseudo struttura ECGPreProcessing.py

--- ECGReader/ECGPreProcessing.py
+++ ECGReader/ECGPreProcessing.py
@@ -4,3 +4,27 @@ Creates mask etc.
 import cv2
 import SPxml
 import numpy as np
+
+def dataToArray():
+    """
+    Receives .png and .xml as input
+    Load .png and .xml
+    Returns: nparray_png[i] is the image corresponding to traces in np_array_xml[i]
+
+    nparray_png[numero di dati (78), h, w]
+    nparray_xml[numero di dati (78), num traccia, dati traccia]
+    """
+    pass
+def match():
+    pass
+def trasformazioni():
+    pass
+def processData():
+    """
+    nparray_png, nparray_xml
+
+    nparray_xml -> numero dati, numero traccia, tempo, h 
+    h = ? -> h_ecg/6 scalare i valori traccia
+        "colorare un pixel"
+    """
+    pass
