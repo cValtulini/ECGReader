3d0af126548a585f787cdb230bc0f348c2be45cf
nerico <enricocolosio@gmail.com>
2022-01-06 16:01:30 +0100

    Update ECGPreProcessing.py

--- ECGReader/ECGPreProcessing.py
+++ ECGReader/ECGPreProcessing.py
@@ -40,12 +40,12 @@ def loadMatches(file_names,path_to_png_matches,path_to_xml_matches):
     xmls=[]
 
     png_folder =  sorted([file.path for file in os.scandir(path_to_png_matches)
-                        if file_names.count(file.name.split('.')[0])>0])
+                        if list(file_names).count(file.name.split('.')[0])>0])
     for png_path in png_folder:
         pngs.append(loadPNG(png_path))
 
     xml_folder = sorted([file.path for file in os.scandir(path_to_xml_matches)
-                        if file_names.count(file.name.split('.')[0])>0])
+                        if list(file_names).count(file.name.split('.')[0])>0])
     for xml_path in xml_folder:
         xmls.append(loadXML(xml_path))
 
