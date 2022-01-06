517166041ed62221418b7533d88c4a0705207523
nerico <enricocolosio@gmail.com>
2022-01-06 14:54:39 +0100

    Update ECGPreProcessing.py

--- ECGReader/ECGPreProcessing.py
+++ ECGReader/ECGPreProcessing.py
@@ -51,7 +51,7 @@ def loadMatches(file_names,path_to_png_matches,path_to_xml_matches):
     #pngs.append(loadPNG(png) for png in png_folder)
 
     xml_folder = sorted([file.path for file in os.scandir(path_to_xml_matches)
-                        if file_names.count(file.name.name.split('.')[0])>0])
+                        if file_names.count(file.name.split('.')[0])>0])
     #xmls.append(loadXML(xml)[:, :5450] for xml in xml_folder)
     for xml_path in xml_folder:
         pngs.append(loadXML(xml_path)[:, :5450])
