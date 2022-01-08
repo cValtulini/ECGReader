3d73e288820320d19e238c6cedd7ce3c4b734782
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-08 20:18:46 +0100

    implemented extractWaveformMasks()

--- ECGReader/ECGPreProcessing.py
+++ ECGReader/ECGPreProcessing.py
@@ -3,8 +3,8 @@ Creates mask etc.
 """
 import os
 import cv2
-import SPxml
 import numpy as np
+from tqdm import tqdm
 
 
 # Multiplies the '-' character that separates sections of outputs
@@ -12,197 +12,116 @@ import numpy as np
 _string_mult = 100
 
 
-def loadXML(path_to_file):
+def loadPNG(path_to_file, binary=False):
     """
-    Loads a single XML
-    """
-
-    ecg = SPxml.getLeads(path_to_file)
-    return np.array([ecg[i]['data'] for i in range(len(ecg))])#[:, :5450]
-
-
-def loadPNG(path_to_file):
-    """
-    Loads a single PNG
+    Loads a single PNG, returns it grayscale color-inverted or unchanged
     """
 
-    image = cv2.imread(path_to_file, cv2.IMREAD_GRAYSCALE)
+    if binary: return cv2.imread(path_to_file, cv2.IMREAD_UNCHANGED)
+    else: return 255 - cv2.imread(path_to_file, cv2.IMREAD_GRAYSCALE)
 
-    return 1 - (image / 255)
 
-
-def loadMatches(file_names, path_to_png_matches, path_to_xml_matches):
+def loadMatches(file_paths):
     """
     Loads file in matches folder, requires a list of file names without 
     file extensions and the paths for the two folders for the files
     to match.
     """
 
-    #return pngs, xmls generator
-    for file in file_names:
-        png_file = f'{path_to_png_matches}/{file}.png'
-        xml_file = f'{path_to_xml_matches}/{file}.xml'
-
-        yield (loadPNG(png_file), loadXML(xml_file))
-
-"""
-def loadUnmatched(path_to_files, extension):
-    """ """
-    !!! Input is a list of paths, not names as in matches
-    """ """
-    
-    assert extension == 'png' or extension == 'xml', 'Extension not recognised'
-
-    if extension == 'png':
-        return np.stack([loadPNG(png) for png in path_to_files], axis=0)
-    elif extension == 'xml':
-        return np.stack([loadXML(xml) for xml in path_to_files], axis=0)
-"""
+    #return a pngs generator
+    for file in file_paths:
+        yield loadPNG(file)
 
 
-# XML Loading is fast we could load them just to find these params
-def findMaxRange(xml_array):
-    """
-    
-    """
-
-    return (xml_array.max(axis=-1) - xml_array.min(axis=-1)).max()
-
-
-def loadData(path_to_png, path_to_xml):
+def loadData(path_to_png):
     """
     Expect the two paths to only contain folders `matches` and `unmatched`, and at least `matches` to contain elements
     Returns a list containing the proper number of arrays based on folders and folders content ordered as png_matches xml_matches 
     """
 
     assert sorted([_.name for _ in os.scandir(path_to_png) if _.is_dir()]) == sorted(['matches', 'unmatched']), 'PNG folder content is not as expected.'
-    assert sorted([_.name for _ in os.scandir(path_to_xml) if _.is_dir()]) == sorted(['matches', 'unmatched']), 'XML folder content is not as expected.'
 
     png_matches_path = f'{path_to_png}/matches'
-    xml_matches_path = f'{path_to_xml}/matches'
-    #png_unmatched_path = f'{path_to_png}/unmatched'
-    #xml_unmatched_path = f'{path_to_xml}/unmatched'
-    #_existing_unmatched_png=False
-    #_existing_unmatched_xml=False
-
-    # Check if there are files in the unmatched folders
-    """
-    if len([_ for _ in os.scandir(png_unmatched_path) if _.is_file()]):
-        _existing_unmatched_png = True
-    if len([_ for _ in os.scandir(xml_unmatched_path) if _.is_file()]):
-        _existing_unmatched_xml = True
-    """
 
     # Reads matches folders
     png_matches = sorted(
-        [_.name.split('.')[0] for _ in os.scandir(png_matches_path)
-        if len(_.name.split('.')) == 2 and _.name.split('.')[1] == 'png'])
-    xml_matches = sorted(
-        [_.name.split('.')[0] for _ in os.scandir(xml_matches_path)
-        if len(_.name.split('.')) == 2 and _.name.split('.')[1] == 'xml'])
-
-    matches = set(png_matches).intersection(xml_matches)
-    # set: png_matches - xml_matches
-    unmatched_matches_png = set(png_matches).difference(xml_matches)
-    # set: xml_matches - png_matches
-    unmatched_matches_xml = set(xml_matches).difference(png_matches) 
-    
-    # Checks if there are unmatched files in the matches folders, tells the 
-    # user if so
-    if len(unmatched_matches_png) or len(unmatched_matches_xml):
-        print('You have unmatched files in the unmatched folders!')
-        print(unmatched_matches_png)
-        print(unmatched_matches_xml)
-        print('They\'ll be ignored')
+        [_.path for _ in os.scandir(png_matches_path)
+        if len(_.name.split('.')) == 2 and _.name.split('.')[1] == 'png']
+        )
 
     print('-' * _string_mult)
     print('Loading files...')
 
     data = dict()
 
-    # matched_pngs, matched_xmls = 
-    matches_generator = loadMatches(matches,
-        png_matches_path,
-        xml_matches_path
-        )
+    matches_generator = loadMatches(png_matches)
     data.update({"matches": matches_generator})
-    #data.update({"matches_png_files":matched_pngs})
-    #data.update({"matches_xml_files":matched_xmls})
 
-    """
-    # Load unmatched files if it's the case to do so
-    if _existing_unmatched_png:
-        png_unmatched = sorted(
-            [_.path for _ in os.scandir(png_unmatched_path)
-            if len(_.name.split('.')) == 2 and _.name.split('.')[1] == 'png']
-            )
-        #data.append(loadUnmatched(png_unmatched, 'png'))
-        data.update({"unmatched_png_files":loadUnmatched(png_unmatched, 'png')})
-    if _existing_unmatched_xml:
-        xml_unmatched = sorted(
-            [_.path for _ in os.scandir(xml_unmatched_path)
-            if len(_.name.split('.')) == 2 and _.name.split('.')[1] == 'xml']
-            )
-        #data.append(loadUnmatched(xml_unmatched, 'xml'))
-        data.update({"unmatched_xml_files":loadUnmatched(xml_unmatched, 'xml')})
-    """
-
-    print('-' * _string_mult)
     print('Completed.')
-
-    matches_number = len(matches)
-
+    print('-' * _string_mult)
     
-    ecg_max_lead_range = findMaxRange(
-        np.stack(
-            [loadXML(f'{png_matches_path}/{file}.xml') for file in matches],
-            axis=0)
-            )
-
-    return data, matches_number, ecg_max_lead_range 
+    return data, png_matches
 
 
-# Should we return a 0-255 (?)
-def createWaveformMask(ecg_lead, span):
+def gridRemoval(img, std_coeff):
     """
     
     """
-    # Creates the mask image background with width = the number of time samples
-    # and height = span*10 since we know the resolution is 0.1 -> 1/0.1 is the 
-    # expansion factor we need to map values to integers (pixel's positions)
-    mask = np.zeros(shape=(int(span*10), ecg_lead.shape[-1]), dtype=np.uint8)
 
-    # Creates the vector of indexes to map the digital data onto the image 
-    # (mask)
-    # We have a range of values going from negative to positive, but in 
-    # the image the top left position is 0 and indexes grow moving towards the 
-    # bottom left -> we subtract out lead's maximum value to map it to 0 and # invert the sign, we then multiply by the expansion coefficient and cast 
-    # to int for indexing 
-    indexes = (-(ecg_lead-ecg_lead.max())*10).astype(np.uint16)
+    while np.sum(img > img.mean() + std_coeff*img.std()) == 0:
+        std_coeff -= 0.05
 
-    # index over the computed indexes coupled with a "time axis" to set to 1 
-    # pixel corresponding to waveform points
-    mask[indexes, np.arange(0, ecg_lead.shape[-1])] = 1
-
-    return mask
+    return (img > img.mean() + std_coeff*img.std()).astype(np.uint8)
 
 
-#, unmatched_images=None, unmatched_ecg_leads=None):
-def findECGBestMatch(matches):
+def adjustTemplate(template):
+    """
+    Cut additional background from a binary template
     """
 
+    limits = np.where(temp > 0)
+    return template[limits[0].min():limits[0].max()+1,
+                limits[1].min():limits[1].max()+1]
+
+
+def extractWaveformMasks(path_to_ecgs, path_to_templates, path_for_masks):
     """
 
-    # Computes parameters to create waveform masks
-    
+    N.B. : this version of the function assumes only one template
+    """
 
-        # Creates waveform masks from leads
-        
-        # waveform mask has to be transformed in different ways
-        
-        # Compare image per image with waveform mask(s)
-        
-        # Find best match
+    data, matches_path = loadData(path_to_ecgs)
+    gen = data["matches"]
 
+    temp = adjustTemplate(loadPNG(f'{path_to_templates}/triangle_temp.png'))
 
-    pass
+    print('-'* _string_mult)
+
+    for match in tqdm(matches_path, desc='Creating masks'):
+        mask = gridRemoval(next(gen), 5)
+
+        #eventually this could be removed from both image and mask
+        mask[:, -321:] = 0 
+        mask[:, 4338:4428] = 0
+
+        try:
+            res = cv2.matchTemplate(mask[:100], temp, cv2.TM_CCORR_NORMED)
+            
+            template_locations = np.where(res > res.max()*0.9)
+            row = template_locations[0]
+            col = template_locations[1]
+            
+            prev_x, prev_y = (10000, 10000)
+            for x, y in zip(row, col):
+                # Should avoid removing pixels unnecessarily, we haven't move
+                # at least two pixels far from the previous location doesn't 
+                # remove pixels
+                if (prev_x-x) + (prev_y-y) > 2:
+                    mask[:100][x:x+temp.shape[0], y:y+temp.shape[1]] = 0
+                
+                prev_x, prev_y = (x, y)
+
+        except Exception:
+            continue
+
+        cv2.imwrite(f'{path_for_masks}/{match}.png', mask, [cv2.IMWRITE_PNG_BILEVEL, 1])
