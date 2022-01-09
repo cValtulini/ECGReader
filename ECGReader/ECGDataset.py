f03e7bb884a6d04efa69af682967d17cf939503d
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-09 10:42:27 +0100

    created ECGDataset.py file

---
+++ ECGReader/ECGDataset.py
@@ -0,0 +1,20 @@
+"""
+Defines the main utilities for loading the ECGDataset
+"""
+# imports
+
+# maybe utilities functions
+
+_string_mult = 100
+
+
+class ECGDataset(object): #or maybe it should inherit from some dataset class from tensorflow
+
+    def __init__(self):
+        pass
+
+    def foo(self, bar):
+        pass
+    
+    def load(self):
+        pass
