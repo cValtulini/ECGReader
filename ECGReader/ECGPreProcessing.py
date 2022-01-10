5c1b2a1b24746138faf57842c9833934658e47b0
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-10 11:45:17 +0100

    updated documentation for ECGPreProcessing.py

--- ECGReader/ECGPreProcessing.py
+++ ECGReader/ECGPreProcessing.py
@@ -7,13 +7,29 @@ import numpy as np
 from tqdm import tqdm
 
 
-# Multiplies the '-' character that separates sections of outputs for some functions
+# Multiplies the '-' character that separates sections of outputs
 _string_mult = 100
 
 
 def loadPNG(path_to_file, binary=False):
     """
-    Loads a single PNG, returns it grayscale color-inverted or unchanged
+    loadPNG loads a png with opencv `imread` given the path.
+    
+    Parameters
+    ----------
+    path_to_file: String
+        The path to the image to be loaded, comprehensive of filename and extension.
+        
+    binary: bool
+        If True loads a binary image without any changes; if False loads a color image 
+        converting it to grayscale and color inverting it (255 - grayscale)
+        Default to False
+
+    Returns
+    -------
+    : numpy.ndarray
+        A numpy array representing the image
+
     """
 
     if binary:
@@ -22,24 +38,53 @@ def loadPNG(path_to_file, binary=False):
         return 255 - cv2.imread(path_to_file, cv2.IMREAD_GRAYSCALE)
 
 
-def loadMatches(file_paths):
+def loadPNGsFromFolder(file_paths):
+    """
+    loadPNGsFromFolder returns an iterator yielding PNG images of the ECGs, grayscale
+    and color inverted.
+
+    Parameters
+    ----------
+    file_paths:
+        List of PNG files paths, comprehensive of filename and extension
+
+    Returns
+    -------
+    : generator
+        A generator yielding PNG files
+
+    """
     """
     Loads file in matches folder, requires a list of file names without file extensions
     and the paths for the two folders for the files to match.
     """
 
-    # return a pngs generator
     for file in file_paths:
         yield loadPNG(file)
 
 
 def loadData(path_to_png):
     """
-    Expect the two paths to only contain folders `matches` and `unmatched`, and at least
-    `matches` to contain elements.
-    Returns a list containing the proper number of arrays based on folders and folders
-    content ordered as png_matches xml_matches
+    loadData returns a dictionary containing generators of PNG files and a list of sorted
+    file paths
+
+    Parameters
+    ----------
+    path_to_png: String
+        The folder to the path expected to contain sub-folders `matches` and
+        `unmatched` containing (at least matches) PNG files.
+        Note: the current version just returns files from the matches folder
+
+    Returns
+    -------
+    data: Dictionary
+        Containing keys `matches` and `unmatched` indexing the generators for the
+        corresponding folder's sorted PNG files
+    png_matches: List[String]
+        The list of PNG file's paths. comprehensive of filename and extension
+
     """
+    # TODO: Implement loading from the `unmatched` folder
 
     assert sorted([_.name for _ in os.scandir(path_to_png) if _.is_dir()]) == sorted([
             'matches', 'unmatched']), 'PNG folder content is not as expected.'
@@ -50,14 +95,14 @@ def loadData(path_to_png):
     png_matches = sorted(
         [_.path for _ in os.scandir(png_matches_path)
          if len(_.name.split('.')) == 2 and _.name.split('.')[1] == 'png']
-    )
+        )
 
     print('-' * _string_mult)
     print('Loading files...')
 
     data = dict()
 
-    matches_generator = loadMatches(png_matches)
+    matches_generator = loadPNGsFromFolder(png_matches)
     data.update({"matches": matches_generator})
 
     print('Completed.')
@@ -67,6 +112,26 @@ def loadData(path_to_png):
 
 
 def gridRemoval(img, std_coeff):
+    """
+    gridRemoval removes the grid from color inverted PNG files of ECGs based on the
+    image grayscale values mean and standard deviation (multiplied by a coefficient).
+    While the output binary image sum is 0 progressively reduces the coefficient
+    multiplying the standard deviation.
+
+    Parameters
+    ----------
+    img: numpy.ndarray, shape=(W, H)
+        The grayscale image, assuming it is color inverted
+
+    std_coeff: float
+        The coefficient multiplying the standard deviation
+
+    Returns
+    -------
+    : numpy.ndarray, shape=(W, H), dtype=np.uint8
+        An array of 0/1 values with 0 for background and 1 for the signal
+
+    """
     """
     
     """
@@ -79,7 +144,22 @@ def gridRemoval(img, std_coeff):
 
 def adjustTemplate(template):
     """
-    Cut additional background from a binary template
+    adjustTemplate given an image as a numpy array resize it removing eventual border
+    where
+    all pixels
+    on the border's line are 0.
+
+    Parameters
+    ----------
+    template: numpy.ndarray
+        The image to cut
+
+    Returns
+    -------
+    : numpy.ndarray
+        The input array without external border where all the border's line is made of
+        0 values
+
     """
 
     limits = np.where(template > 0)
@@ -88,9 +168,26 @@ def adjustTemplate(template):
 
 def extractWaveformMasks(path_to_ecgs, path_to_templates, path_for_masks):
     """
+    extractWaveformMasks saves PNG files of waveform masks extracted from PNG of ECGs
+    extracted from digitally printed PDFs.
 
-    N.B. : this version of the function assumes only one template
+    Parameters
+    ----------
+    path_to_ecgs: String
+        The path to the folder containing PNG images to load and from which to extract
+        waveform masks, have to be divided into `matches` and `unmatched`
+    path_to_templates: String
+        Path to the folder containing templates of element to be removed from the
+        waveform mask after grid removal
+    path_for_masks: String
+        Path to the folder in which to save the generated waveform mask
+
+    Returns
+    -------
+    Nothing
+
     """
+    # TODO: if needed modify to use more than just one template
 
     data, matches_path = loadData(path_to_ecgs)
     gen = data["matches"]
@@ -100,24 +197,32 @@ def extractWaveformMasks(path_to_ecgs, path_to_templates, path_for_masks):
     print('-' * _string_mult)
 
     for match in tqdm(matches_path, desc='Creating masks'):
+        # Asks the generator to return the next element and directly passes it to the
+        # gridRemoval function
         mask = gridRemoval(next(gen), 5)
 
-        # eventually this could be removed from both image and mask
+        # TODO: [If necessary] remove the second part completely from images and masks
+        #  instead of just setting it to 0
         mask[:, -321:] = 0
         mask[:, 4338:4428] = 0
 
         try:
+            # Looks for the template shape inside the image
             res = cv2.matchTemplate(mask[:100], temp, cv2.TM_CCORR_NORMED)
 
+            # Get regions where the value of the correlation image is greater than a
+            # certain threshold based on the maximum value of res image (which is
+            # normalized)
+            # TODO: [If necessary] since `res` is normalized it could be better to use
+            #  a coefficient between 0 and 1 alone instead of multiplying it by res.max()
             template_locations = np.where(res > res.max() * 0.9)
             row = template_locations[0]
             col = template_locations[1]
 
-            prev_x, prev_y = (10000, 10000)
+            prev_x, prev_y = (2 * mask.shape[0], 2 * mask.shape[1])
             for x, y in zip(row, col):
-                # Should avoid removing pixels unnecessarily, we haven't move
-                # at least two pixels far from the previous location doesn't 
-                # remove pixels
+                # Should avoid removing pixels unnecessarily, if it hasn't moved at
+                # least two pixels far from the previous location doesn't remove pixels
                 if (prev_x - x) + (prev_y - y) > 2:
                     mask[:100][x:x + temp.shape[0], y:y + temp.shape[1]] = 0
 
