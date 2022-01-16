a2d1e7c2876f768f853aaf3aa128f39bda334301
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-16 14:51:57 +0100

    implemented patch division for datasets

--- ECGReader/ECGModel.py
+++ ECGReader/ECGModel.py
@@ -20,104 +20,123 @@ def show(img):
 
 if __name__ == '__main__':
     # Load images and masks as tf.data.Dataset loading from ImageDataGenerator
-    # Pass the data folder as an argument when calling the script, later it can be the
-    # argument of a function call
     _, path = argv
-    images_path = os.path.join(path, 'png/matches')
+    ecg_path = os.path.join(path, 'png/matches/img')
+    masks_path = os.path.join(path, 'png/matches/masks')
+
+    original_width = 9082
+    original_height = 4410
+
+    patch_size = 256
+    stride_size = 64
+
+    new_shape = (patch_size*int((original_height//2)//patch_size),
+                 patch_size*int((original_width//2)//patch_size))
 
     # Creating a generator to load images for the Dataset
-    # For now I'm just trying to set up thing but we have a decision to make that will
-    # impact the way we set up this process: how do we divide train and test set
-    # If we divide them through the folders then it will be pretty straight forward,
-    # we can create separate ImageDataGenerator with different parameters (this is true
-    # for the subdivision of train and validation too). Otherwise we have to look up (
-    # or find a way to) how to properly divide data when loading them this way.
-    # Validation split divides the data that are loaded, I'm actually using it now for
-    # what I'm calling test_set, planning to furtherly divide train_set into train and
-    # validation (even though I still don't know how I'm going to do it.
-    images_gen = ImageDataGenerator(
-        rescale=1. / 255, validation_split=0.2
+    ecg_gen = ImageDataGenerator(
+        rescale=1. / 255
+        )
+    masks_gen = ImageDataGenerator(
+        rescale=1. / 255
         )
 
-    # I just need these two objects to set up output_shapes and output_types of the
-    # function that creates the datasets later.
-    # As target size I've decided to subsample with a factor 1/3 the original size of
-    # images (we may want to look up how this subsampling is performed). class_mode
-    # None specifies we don't want labels based on the folder in which data are
-    # contained to be returned when calling flow_from_directory, seed is the usual
-    # parameter that initializes random things and subset selects which of the two
-    # between train and validation set we want (from validation_split parameter)
-    train_images = next(
-        images_gen.flow_from_directory(
-            images_path, target_size=(1408, 3072), class_mode=None, seed=42,
-            subset='training'
+    # We need train_images.shape and train_images.dtype (same for test_images) as
+    # parameters of `tf.data.Dataset.from_generator`
+    ecg_images = next(
+        ecg_gen.flow_from_directory(
+            ecg_path, target_size=new_shape, class_mode=None, seed=42,
+            color_mode='grayscale', batch_size=1
             )
         )
-    test_images = next(
-        images_gen.flow_from_directory(
-            images_path, target_size=(1408, 3072), class_mode=None, seed=42,
-            subset='validation'
+    masks_images = next(
+        masks_gen.flow_from_directory(
+            masks_path, target_size=new_shape, class_mode=None, seed=42,
+            color_mode='grayscale', batch_size=1
             )
         )
 
     print('-' * _string_mult)
+    print('Shapes:')
+    print(ecg_images.shape)
+    print(masks_images.shape)
+
+    print('-' * _string_mult)
     print('Loading sets')
 
-    # Creating the data set from the ImageDataGenerator objects I previously
-    # initialized, I pass flow_from_directory as a lambda function to avoid passing it
-    # as an object because I would have needed to pass its parameters as an additional
-    # list parameter of from_generator (haven't tried so I don't actually know how to
-    # properly set that up)
-    train_set = tf.data.Dataset.from_generator(
-        lambda: images_gen.flow_from_directory(
-            images_path, target_size=(1408, 3072),
-            class_mode=None, seed=42,
-            subset='training'
+    # Creating the data set from the ImageDataGenerator objects we previously
+    # initialized.
+    ecg_set = tf.data.Dataset.from_generator(
+        lambda: ecg_gen.flow_from_directory(
+            ecg_path, target_size=new_shape,
+            class_mode=None, seed=42, color_mode='grayscale', batch_size=1
             ),
-        output_types=train_images.dtype,
-        output_shapes=train_images.shape
+        output_types=ecg_images.dtype,
+        output_shapes=ecg_images.shape,
+        name='ecg'
         )
-    test_set = tf.data.Dataset.from_generator(
-        lambda: images_gen.flow_from_directory(
-            images_path, target_size=(1408, 3072),
-            class_mode=None, seed=42,
-            subset='validation'
+    masks_set = tf.data.Dataset.from_generator(
+        lambda: masks_gen.flow_from_directory(
+            masks_path, target_size=new_shape,
+            class_mode=None, seed=42, color_mode='grayscale', batch_size=1
             ),
-        output_types=test_images.dtype,
-        output_shapes=test_images.shape
+        output_types=masks_images.dtype,
+        output_shapes=masks_images.shape,
+        name='mask'
         )
 
     print('Sets loaded.')
     print('-' * _string_mult)
 
-    # The map method applies a function passed as parameter to all the objects of the
-    # set, in this case turning images into grayscale: I haven't found (or looked for,
-    # actually) a way to do this automatically through ImageDataGenerator but I don't
-    # think it will be a problem...
-    train_set = train_set.map(tf.image.rgb_to_grayscale)
-    test_set = test_set.map(tf.image.rgb_to_grayscale)
-
     # Prints information about the elements of the data_set, in particular dtype and
     # shape, a note about this:
-    # For the train_set it now returns (32, height, width, 1) where 1 is for grayscale
-    # and 32 is for the number of elements, I think this is related to batches division
-    # of the set but I still haven't had the time to look up how to use/manage this.
-    print('Train: ')
-    print(train_set.element_spec)
-    print('Test:')
-    print(test_set.element_spec)
-
-    # Shows examples from the set, still have to understand how take works
-    for image in train_set.take(3):
-        show(image[0, :, :, 0])
+    # According to the guide (cited later) if I understood correctly all the element in
+    # a dataset (batches?) have the same characteristic, element_spec return the
+    # specification of
+    # a single one.
+    print('ECGs:')
+    print(ecg_set.element_spec)
+    print('Masks: ')
+    print(masks_set.element_spec)
 
     # Divide into patches
-    # I found this link and wanted to try this
-    # https://stackoverflow.com/questions/64326029/load-tensorflow-images-and-create-patches
+    ecg_set = ecg_set.map(
+        lambda x: tf.reshape(tf.image.extract_patches(
+                x, sizes=[1, patch_size, patch_size, 1],
+                strides=[1, stride_size, stride_size, 1],
+                rates=[1, 1, 1, 1],
+                padding='VALID'
+                ),
+            [-1, patch_size, patch_size, 1]
+            )
+        )
+    masks_set = masks_set.map(
+        lambda x: tf.reshape(tf.image.extract_patches(
+                x, sizes=[1, patch_size, patch_size, 1],
+                strides=[1, stride_size, stride_size, 1],
+                rates=[1, 1, 1, 1],
+                padding='VALID'
+                ),
+            [-1, patch_size, patch_size, 1]
+            )
+        )
+
+    # Create a single dataset from the two sets
+    ecg_set = ecg_set.unbatch()
+    masks_set = masks_set.unbatch()
+    ecg_masks_set = tf.data.Dataset.zip((ecg_set, masks_set))
+
+    print(ecg_masks_set.element_spec)
 
     # Select patches
-    # I don't know if it's possible (and don't think it is) to just select them through
-    # indexing
+    ecg_masks_set = ecg_masks_set.filter(
+        lambda x, y: tf.math.greater(tf.math.count_nonzero(y), 0)
+        )
+    ecg_masks_set = ecg_masks_set.batch(batch_size=1)
+
+    for ecg, mask in ecg_masks_set:
+        if tf.math.count_nonzero(mask):
+            print('Whooops')
 
     # I think it will be best to apply transformations after selection if we apply them
     # through tf.data.Dataset.map(), I've seen there are a bunch of tf.image functions
@@ -130,11 +149,11 @@ if __name__ == '__main__':
     # Nothing to add from here on, just jotted down the comments to eventually expand,
     # feel free to add modify and do every kind of thing on this code (or do anything
     # else at all) and tell me if I have to merge some commit tomorrow.
-    # I was trying to do all this with info from the tf.keras.preprocessing.image.ImageDataGenerator
-    # page of the tensorflow API + the tf.data.Dataset page + the "Build Tensorflow
-    # input pipelines" guide that you can find linked in both the previous pages (
-    # surely on the Dataset one) see the Preprocessing section of this one for the
-    # visualization part that I just started.
+    # I was trying to do all this with info from the
+    # tf.keras.preprocessing.image.ImageDataGenerator page of the tensorflow API + the
+    # tf.data.Dataset page + the "Build Tensorflow input pipelines" guide that you can
+    # find linked in both the previous pages ( surely on the Dataset one) see the
+    # Preprocessing section of this one for the visualization part that I just started.
 
     # Load model
 
