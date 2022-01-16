b8a5e912d8b990bd60f83175f42bb72229f5cec2
nerico <enricocolosio@gmail.com>
2022-01-16 13:41:27 +0100

    dataset tests

--- ECGReader/ECGDataset.py
+++ ECGReader/ECGDataset.py
@@ -4,6 +4,7 @@ Defines the main utilities for loading the ECGDataset
 import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
+import os
 
 
 _string_mult = 100
@@ -14,14 +15,14 @@ class ECGDataset(object):
 
     # TODO: Should call load or something like that to load ecgs and masks, should also
     #  implement "distortion" parameters as function parameters
-    def __init__(self, parameters):
+    def __init__(self):
         # TODO: they will be keras.preprocessing.ImageDataGenerator object(s)
         # TODO: when loading masks check if it's needed to modify their creation to use
         #  values in 0-255
         # TODO: we should definitely implement a way to divide images into patches:
         #  check cv2 or skimage
-        self.ECGs = None
-        self.masks = None
+        self.ECGs= None
+        #self.masks = None
 
 
     def foo(self, bar):
@@ -29,4 +30,7 @@ class ECGDataset(object):
 
 
     def loadDataset(self):
+
+
+
         pass
