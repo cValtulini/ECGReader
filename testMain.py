79d6665a9e43a0a13c03ebd8b7999a74e0cf4e8a
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-17 18:11:54 +0100

    removed old unnecessary files

--- testMain.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-
-script, arg1, arg2 = argv
-
-print(f"Script: {script}")
-print(f"Argument1: {arg1}")
-print(f"Argument2: {arg2}")
-print("Hello, World!")
--- testModule.py
+++
@@ -1,2 +0,0 @@
-def testFunction():
-    print("Hello, World!")
