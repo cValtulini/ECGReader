2477ceda6d0fb98d21144d852237e04893e364b6
cValtulini <c.valtulini002@studenti.unibs.it>
2022-01-25 15:41:02 +0100

    added chance to load pretrained model

--- ECGReader/ECGModel.py
+++ ECGReader/ECGModel.py
@@ -118,7 +118,7 @@ class ECGModel(object):
 
         # Entry block
         x = layers.Conv2D(
-            16, 3,  # strides=2,
+            32, 3,  # strides=2,
             padding="same"
             )(inputs)
         x = layers.BatchNormalization()(x)
@@ -127,7 +127,7 @@ class ECGModel(object):
         x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
 
         # Blocks 1, 2, 3 are identical apart from the feature depth.
-        for filters in [32, 64, 128]:
+        for filters in [64, 128]:
             x = layers.Activation(layers_activation)(x)
             x = layers.SeparableConv2D(filters, 3, padding="same")(x)
             x = layers.BatchNormalization()(x)
@@ -144,7 +144,7 @@ class ECGModel(object):
         """
 
         print(previous_block_activation)
-        for i, filters in enumerate([128, 64, 32, 16]):
+        for i, filters in enumerate([128, 64, 32]):
             x = layers.Activation(layers_activation)(x)
             x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
             x = layers.BatchNormalization()(x)
