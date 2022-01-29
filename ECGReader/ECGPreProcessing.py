ef09b9c610fc7afb8a9c3bee4e2dc0e8910c72e5
nerico <enricocolosio@gmail.com>
2022-01-29 18:41:30 +0100

    Update ECGPreProcessing.py

--- ECGReader/ECGPreProcessing.py
+++ ECGReader/ECGPreProcessing.py
@@ -2,7 +2,7 @@
 Creates mask etc.
 """
 import os
-# import SPxml
+import SPxml
 import numpy as np
 from matplotlib import pyplot as plt
 from imgaug import augmenters as iaa
@@ -13,7 +13,8 @@ _string_mult = 100
 
 def plotXML(tracks, fullname, sim, destination_path):
     """
-    Plots a single xml given its tracks and based on the fact that is a simultaneous record or sequential.
+    Plots a single xml given its tracks and based on the fact that is a simultaneous record 
+    or sequential.
 
     Parameters
     ----------
@@ -58,7 +59,9 @@ def plotXML(tracks, fullname, sim, destination_path):
 
     for i in range(col):
         for j in range(row):
-
+            
+            # We need to set different ranges for the x axis in order to match the printed ECG size,
+            # 2500 values in the first column, 2640 in the second.
             if i > 0:
                 # major_ticks_x = np.arange(0, 2640, 100)
                 # minor_ticks_x = np.arange(0, 2640, 20)
@@ -79,6 +82,8 @@ def plotXML(tracks, fullname, sim, destination_path):
             # ax[j][i].grid(which='minor', alpha=0.2)
             # ax[j][i].grid(which='major', alpha=0.5)
             # ax[j][i].set_aspect(10, 'box')
+
+            # Hiding spines and ticks to get a clear mask.
             ax[j][i].spines['top'].set_visible(False)
             ax[j][i].spines['right'].set_visible(False)
             ax[j][i].spines['bottom'].set_visible(False)
@@ -90,6 +95,7 @@ def plotXML(tracks, fullname, sim, destination_path):
 
             t += 1
 
+    # We save different ECG formats in different folders using the sim variable.
     if sim:
         plt.savefig(
             destination_path + fullname.split("/")[-1].split(".")[0] + ".png", dpi=200,
@@ -126,27 +132,33 @@ def masksPlotterXML(source_png_path, source_xml_path, destination_path):
 
     sim = False
     xml_names = []
+
+    # Gathering file names in PNG folder to match them with the corresponding XMLs.
     seqs = sorted(
         [_.name for _ in os.scandir(source_png_path)]
         )
-
     for seq in seqs:
         name = seq.split(".")[0] + ".xml"
         xml_names.append(source_xml_path + name)
 
+    # Setting the variable to differentiate simultaneous and sequential ECGs. 
     if source_png_path.split("/")[-1] == "sim":
         sim = True
 
+    # Iterating over XMLs to get their tracks and plot them.
     for xml in xml_names:
 
         datas = SPxml.getLeads(xml)
         tracks = []
 
+        # In case of simultaneous ECG we get the samples for the first 2460 samples for each track.
         if sim:
             for i in range(len(datas)):
                 tracks.append(np.array(datas[i]['data']))
                 tracks[i] = tracks[i][:2460] - np.around(tracks[i][:2460].mean(), 1)
 
+        # In case of sequential ECG we get the first 2460 samples for the first 6 tracks, and then the
+        # samples from 2500 to 5000 for the others.
         else:
             for i in range(len(datas)):
                 tracks.append(np.array(datas[i]['data']))
